44cb4f123164f5f14922cd9734c6d67ff4f3a3ec
Alan <alanicio98@outlook.com>
2020-09-22 01:46:37 -0500

    photo preview

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class AddUserComponent implements OnInit {
 	@Input() displayModal:boolean;
 	@Input() displayHandler:Function;
+	public imageSrc;
 	constructor() { }
 
 	ngOnInit(): void {
@@ -20,6 +21,18 @@ export class AddUserComponent implements OnInit {
 		else{
 			return "none";
 		}
+	}
+
+	readURL(event: Event): void {
+		let files=(event.target as HTMLInputElement).files;
+	    if (files && files[0]) {
+	        const file = files[0];
+
+	        const reader = new FileReader();
+	        reader.onload = e => this.imageSrc = reader.result;
+
+	        reader.readAsDataURL(file);
+	    }
 	}
 
 }
